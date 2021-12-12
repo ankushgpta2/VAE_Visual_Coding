697b371389cf8b3e3195f1d9af242604f975adbc
Ankush Gupta <84059577+ankushgpta2@users.noreply.github.com>
2021-12-12 16:32:41 -0500

    Main Script For Creating Sim Data + Running VAE

---
+++ main.py
@@ -0,0 +1,25 @@
+from dnn_structures import *
+from artificial_data import *
+from handle_dnn_inputs import *
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+import pandas as pd
+import glob
+from sklearn.metrics import make_scorer, mean_squared_error
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Dense, Input, BatchNormalization, LeakyReLU, Flatten, GaussianDropout, Lambda
+import matplotlib.pyplot as plt
+
+# get hyperparameters
+general_parameters, vae_parameters, artificial_info = get_hyperparams()
+
+# ----------------------------------------------- ARTIFICIAL DATA THROUGH VAE ------------------------------------------------
+# whether or not to run on artificial data
+if artificial_info['run_artificial'] is True:
+    artificial_main(repeat=False)
+else:
+    pass
